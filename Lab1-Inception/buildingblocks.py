f7257116d5aba816a89e4d9272e1b7b41752a84d
Roman Polishchenko <rpoli@softserveinc.com>
2022-10-31 10:55:59 +0200

    added nn model based on inception blocks architecture

---
+++ Lab1-Inception/buildingblocks.py
@@ -0,0 +1,74 @@
+from typing import Any, Optional, Callable
+
+from dataclasses import dataclass
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, **kwargs: Any) -> None:
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
+
+
+@dataclass
+class InceptionConfig:
+    """Class for storing configuration of specific Inception block with dimension reductions
+     e.g. number of filters in each conv/pool branch"""
+    num_1x1: int
+    num_3x3_reduce: int
+    num_3x3: int
+    num_5x5_reduce: int
+    num_5x5: int
+    pool_proj: int
+
+
+class InceptionBlock(nn.Module):
+    """Inception module with dimension reductions according to https://arxiv.org/abs/1409.4842"""
+    def __init__(
+            self,
+            in_channels: int,
+            config: InceptionConfig,
+            conv_block: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(InceptionBlock, self).__init__()
+        if conv_block is None:
+            conv_block = BasicConv2d
+
+        self.branch_1x1 = conv_block(in_channels, config.num_1x1, kernel_size=1)
+
+        self.branch_3x3_1 = conv_block(in_channels, config.num_3x3_reduce, kernel_size=1)
+        self.branch_3x3_2 = conv_block(config.num_3x3_reduce, config.num_3x3, kernel_size=3, padding=1)
+
+        self.branch_5x5_1 = conv_block(in_channels, config.num_5x5_reduce, kernel_size=1)
+        self.branch_5x5_2 = conv_block(config.num_5x5_reduce, config.num_5x5, kernel_size=5, padding=2)
+
+        self.branch_pool_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.branch_pool_2 = conv_block(in_channels, config.pool_proj, kernel_size=1)
+
+    def _forward(self, x: Tensor) -> list[Tensor]:
+        branch_1x1 = self.branch_1x1(x)
+
+        branch_3x3 = self.branch_3x3_1(x)
+        branch_3x3 = self.branch_3x3_2(branch_3x3)
+
+        branch_5x5 = self.branch_5x5_1(x)
+        branch_5x5 = self.branch_5x5_2(branch_5x5)
+
+        branch_pool = self.branch_pool_1(x)
+        branch_pool = self.branch_pool_2(branch_pool)
+
+        outputs = [branch_1x1, branch_3x3, branch_5x5, branch_pool]
+        return outputs
+
+    def forward(self, x: Tensor) -> Tensor:
+        outputs = self._forward(x)
+        return torch.cat(outputs, 1)
---
+++ Lab1-Inception/model.py
@@ -0,0 +1,123 @@
+from typing import Optional
+
+from buildingblocks import InceptionConfig, InceptionBlock, BasicConv2d
+from torch import nn, Tensor
+import torch
+
+
+inception_config_dict = {
+    '3a': InceptionConfig(num_1x1=64, num_3x3_reduce=96, num_3x3=128, num_5x5_reduce=16, num_5x5=32, pool_proj=32),
+    '3b': InceptionConfig(num_1x1=128, num_3x3_reduce=128, num_3x3=192, num_5x5_reduce=32, num_5x5=96, pool_proj=64),
+
+    '4a': InceptionConfig(num_1x1=192, num_3x3_reduce=96, num_3x3=208, num_5x5_reduce=16, num_5x5=48, pool_proj=64),
+    '4b': InceptionConfig(num_1x1=160, num_3x3_reduce=112, num_3x3=224, num_5x5_reduce=24, num_5x5=64, pool_proj=64),
+    '4c': InceptionConfig(num_1x1=128, num_3x3_reduce=128, num_3x3=256, num_5x5_reduce=24, num_5x5=64, pool_proj=64),
+    '4d': InceptionConfig(num_1x1=112, num_3x3_reduce=144, num_3x3=288, num_5x5_reduce=32, num_5x5=64, pool_proj=64),
+    '4e': InceptionConfig(num_1x1=256, num_3x3_reduce=160, num_3x3=320, num_5x5_reduce=32, num_5x5=128, pool_proj=128),
+
+    '5a': InceptionConfig(num_1x1=256, num_3x3_reduce=160, num_3x3=320, num_5x5_reduce=32, num_5x5=128, pool_proj=128),
+    '5b': InceptionConfig(num_1x1=384, num_3x3_reduce=192, num_3x3=384, num_5x5_reduce=48, num_5x5=128, pool_proj=128),
+}
+
+
+class InceptionModel(nn.Module):
+    def __init__(
+            self,
+            num_classes: int = 10,
+            transform_input: bool = False,
+            config_dict: Optional[dict] = None,
+            dropout: float = 0.4,
+    ) -> None:
+        super(InceptionModel, self).__init__()
+        if config_dict is None:
+            config_dict = inception_config_dict
+        self.transform_input = transform_input
+
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=1, padding=3)  # original: stride=2
+
+        self.conv2_1 = BasicConv2d(64, 64, kernel_size=1)
+        self.conv2_2 = BasicConv2d(64, 192, kernel_size=3, padding=1)
+
+        self.inception3a = InceptionBlock(in_channels=192, config=config_dict['3a'])
+        self.inception3b = InceptionBlock(in_channels=256, config=config_dict['3b'])
+
+        self.inception4a = InceptionBlock(in_channels=480, config=config_dict['4a'])
+        self.inception4b = InceptionBlock(in_channels=512, config=config_dict['4b'])
+        self.inception4c = InceptionBlock(in_channels=512, config=config_dict['4c'])
+        self.inception4d = InceptionBlock(in_channels=512, config=config_dict['4d'])
+        self.inception4e = InceptionBlock(in_channels=528, config=config_dict['4e'])
+
+        self.inception5a = InceptionBlock(in_channels=832, config=config_dict['5a'])
+        self.inception5b = InceptionBlock(in_channels=832, config=config_dict['5b'])
+
+        self.avg_pool = nn.AvgPool2d(kernel_size=7, stride=1)
+        self.dropout = nn.Dropout(p=dropout)
+        self.fc = nn.Linear(1024, num_classes)
+
+    def _transform_input(self, x: Tensor) -> Tensor:
+        if self.transform_input:
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
+        return x
+
+    def _forward(self, x: Tensor) -> Tensor:
+        # N x 3 x 28 x 28
+        x = self.conv1(x)
+        # x = self.max_pool(x)
+        # N x 64 x 28 x 28
+        x = self.conv2_1(x)
+        x = self.conv2_2(x)
+        # x = self.max_pool(x)
+        # N x 192 x 28 x 28
+        x = self.inception3a(x)
+        # N x 256 x 28 x 28
+        x = self.inception3b(x)
+        # N x 480 x 28 x 28
+        x = self.max_pool(x)
+        # N x 480 x 14 x 14
+        x = self.inception4a(x)
+        # N x 512 x 14 x 14
+        x = self.inception4b(x)
+        # N x 512 x 14 x 14
+        x = self.inception4c(x)
+        # N x 512 x 14 x 14
+        x = self.inception4d(x)
+        # N x 528 x 14 x 14
+        x = self.inception4e(x)
+        # N x 832 x 14 x 14
+        x = self.max_pool(x)
+        # N x 832 x 7 x 7
+        x = self.inception5a(x)
+        # N x 832 x 7 x 7
+        x = self.inception5b(x)
+        # N x 1024 x 7 x 7
+        x = self.avg_pool(x)
+        # N x 1024 x 1 x 1
+        x = self.dropout(x)
+        # N x 1024 x 1 x 1
+        x = torch.flatten(x, 1)
+        # N x 1024
+        x = self.fc(x)
+        # N x num_classes (10)
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self._transform_input(x)
+        x = self._forward(x)
+        return x  # logits
+
+
+def main():
+    model = InceptionModel()
+    test_input = torch.rand((2, 3, 28, 28))
+
+    test_output = model(test_input)
+    print(test_output.shape)
+
+
+if __name__ == '__main__':
+    main()
